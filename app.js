561791b6a30a4b4a0dc21bb01e0b6a746d14bf4b
Esther Araujo <67280022+EstherAraujo@users.noreply.github.com>
2024-09-06 20:45:26 -0300

    Upload dos arquivos de código
    Upload dos arquivos HTML, CSS e JavaScript do projeto.

---
+++ app.js
@@ -0,0 +1,94 @@
+function obterTemasFormatados(jogo) {
+    // Verifica se o jogo possui o atributo "temas" e se é um array
+    if (jogo.temas && Array.isArray(jogo.temas)) {
+      // Junta os elementos do array "temas" em uma única string, separando por vírgula e espaço
+      return jogo.temas.join(', ');
+    } else {
+      return 'Sem temas informados'; // Retorna uma mensagem padrão caso não haja temas
+    }
+  };
+
+function obterAvaliacaoMetacritic(jogo) {
+    // Verifica se o jogo possui a propriedade "avaliacoes" e se ela possui a propriedade "metacritic"
+    if (jogo && jogo.avaliacoes && jogo.avaliacoes.metacritic !== undefined) {
+      return jogo.avaliacoes.metacritic;
+    } else {
+      return 'Avaliação Metacritic não encontrada';
+    }
+};
+
+function obterAvaliacaoJogadores(jogo) {
+    // Verifica se o jogo possui a propriedade "avaliacoes" e se ela possui a propriedade "jogadores"
+    if (jogo && jogo.avaliacoes && jogo.avaliacoes.jogadores !== undefined) {
+      return jogo.avaliacoes.jogadores;
+    } else {
+      return 'Avaliação dos Jogadores não encontrada';
+    }
+};
+
+
+
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Obtem o termo pesquisado e o deixa com letras minúsculas
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase(); 
+
+    // Observa se o campo pesquisa estiver vazio
+    if(!campoPesquisa){
+        section.innerHTML = `<p>Busca vazia.</p>`
+        return; 
+    };   
+
+    // Inicializa uma string vazia para armazenar os resultados, titulo e descricao
+    let resultados = '';
+    let tit = "";
+    let desc = "";
+    let tag = "";
+
+    
+    // Itera sobre cada dado na lista de dados caso não seja uma string vazia
+    for (let dado of dados) {
+        // Transforma os títulos e suas descições em strings com letras minúsculas
+        tit = dado.titulo.toLowerCase();
+        desc = dado.descricao.toLowerCase();
+        tag = dado.tags.toLocaleLowerCase();
+        // Teste se o termo buscado se encontra dentre os títulos do banco de dados
+        if (tit.includes(campoPesquisa) || desc.includes(campoPesquisa) || tag.includes(campoPesquisa)) {
+            console.log("busca encontrada!")
+            // Cria um um novo elemento no html com os resultados
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <img src = ${dado.poster} alt="Poster do ${dado.titulo} style="width:200px;height:300px;">
+                <p class="descricao-meta">${dado.descricao}</p>
+                <p class="descricao-meta"><b>Temas:</b> ${obterTemasFormatados(dado)}</p>
+                <p class="descricao-meta"><b>Avaliação no MetaCritic:</b> ${obterAvaliacaoMetacritic(dado)}/100</p>
+                <p class="descricao-meta"><b>Avaliação dos Jogadores:</b> ${obterAvaliacaoJogadores(dado)}/10</p>
+                <p class="descricao-meta"><b>Desenvolvedor:</b> ${dado.desenvolvedora}</p>
+                <p class="descricao-meta"><b>Ano de Lançamento:</b> ${dado.anoLancamento}</p>
+                <p class="descricao-meta"><b>Preço Médio:</b> R$ ${dado.precoMedio} - os preços podem variar com o tempo.</p>
+                <p class="descricao-meta"><b>Onde Comprar:</b> ${dado.ondeComprar}</p>
+                <a href=${dado.linkCompra} target="_blank">Mais informações</a>
+            </div>
+          `;            
+        };      
+    };
+
+    if (!resultados) {
+        resultados = `
+        <p class="descricao-meta">Nenhum jogo foi encontrado.</p>
+        `
+    }
+  
+    // Atribui os resultados gerados ao HTML da seção
+    section.innerHTML = resultados;
+};
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,242 @@
+let dados = [
+    {
+        titulo: "SimCity 2000",
+        descricao: "Marco nos jogos de construção de cidades. Lançado em 1993, revolucionou o gênero com sua profundidade e realismo. Nele, você assume o papel de prefeito e deve construir e gerenciar uma cidade desde o zero, lidando com problemas como zoneamento, infraestrutura, poluição, desastres naturais e crescimento populacional. O jogo oferece uma experiência imersiva, permitindo que os jogadores experimentem os desafios e recompensas da gestão urbana.",
+        temas: ["construção de cidades", "economia", "desastres naturais"],
+        desenvolvedora: "Maxis",
+        anoLancamento: 1993,
+        avaliacoes: {
+          metacritic: 93,
+          jogadores: 8.5
+        },
+        precoMedio: 9.99,
+        ondeComprar: "GOG",
+        linkCompra: "https://www.gog.com/game/simcity_2000",
+        tags: "cidade antigo famoso economia",
+        poster: "images/simcity-2000-poster.jfif"
+    },
+    {
+      titulo: "Theme Park",
+      descricao: "Jogo de simulação de parque de diversões lançado em 1994. Nele, você é o proprietário de um parque de diversões e precisa construí-lo, gerenciá-lo e garantir a felicidade dos visitantes. O jogo oferece uma variedade de atrações, restaurantes, lojas e paisagismo para personalizar seu parque. É preciso equilibrar o custo das construções com a renda gerada pelos visitantes para manter o parque lucrativo e atraente.",
+      temas: ["parque de diversões", "gerenciamento", "economia"],
+      desenvolvedora: "Bullfrog Productions",
+      anoLancamento: 1994,
+      avaliacoes: {
+        metacritic: 92,
+        jogadores: 8.8
+      },
+      precoMedio: 14.99,
+      ondeComprar: "GOG",
+      linkCompra: "https://www.gog.com/game/theme_park",
+      tags: "diversão antigo montanha russa",
+      poster: "images/theme-park-poster.jpg"
+    },
+    {
+      titulo: "RollerCoaster Tycoon",
+      descricao: "Lançado em 1999, é um dos jogos de simulação de montanha-russa mais populares de todos os tempos. Nele, você projeta e constrói suas próprias montanhas-russas, ajustando a velocidade, altura e inclinação das pistas para criar experiências emocionantes para os visitantes. Além das montanhas-russas, você também precisa gerenciar outros aspectos do parque, como restaurantes, lojas e eventos especiais.",
+      temas: ["montanhas-russas", "parque de diversões", "gerenciamento"],
+      desenvolvedora: "Chris Sawyer Productions",
+      anoLancamento: 1999,
+      avaliacoes: {
+        metacritic: 92,
+        jogadores: 9.1
+      },
+      precoMedio: 12.99,
+      ondeComprar: "Steam",
+      linkCompra: "https://store.steampowered.com/app/212330/RollerCoaster_Tycoon/",
+      tags: "parque diversão antigo famoso montanha russa",
+      poster: "images/Rct-poster.jpg"
+    },
+    {
+      titulo: "Caesar III",
+      descricao: "Jogo de construção de cidades ambientado na Roma Antiga. Lançado em 1998, ele permite que os jogadores construam e gerenciem cidades romanas, desde pequenas aldeias até grandes metrópoles. O jogo oferece uma experiência histórica rica, com a possibilidade de construir monumentos icônicos como o Coliseu e o Fórum Romano. Além da construção, você também precisa lidar com questões políticas, militares e religiosas.",
+      temas: ["construção de cidades", "história", "gerenciamento"],
+      desenvolvedora: "Imperia Games",
+      anoLancamento: 1998,
+      avaliacoes: {
+        metacritic: 91,
+        jogadores: 8.7
+      },
+      precoMedio: 19.99,
+      ondeComprar: "GOG",
+      linkCompra: "https://www.gog.com/game/caesar_iii",
+      tags: "império romano antigo cidade história economia",
+      poster: "images/caesar-III-poster.jfif"
+    },
+    {
+      titulo: "Railroad Tycoon II",
+      descricao: "Jogo de simulação de construção de ferrovias lançado em 1998. Nele, você assume o papel de um magnata ferroviário e precisa construir e gerenciar uma rede ferroviária, transportando passageiros e carga. O jogo oferece um mapa extenso e detalhado, com diversas cidades e recursos naturais para explorar. Você precisará tomar decisões estratégicas sobre rotas, preços e investimentos para construir um império ferroviário lucrativo.",
+      temas: ["transportes", "economia", "estratégia"],
+      desenvolvedora: "Sid Meier",
+      anoLancamento: 1998,
+      avaliacoes: {
+        metacritic: 89,
+        jogadores: 8.4
+      },
+      precoMedio: 16.99,
+      ondeComprar: "GOG",
+      linkCompra: "https://www.gog.com/game/railroad_tycoon_ii",
+      tags: "antigo transporte tráfego economia trem",
+      poster: "images/railroad-tycoon-II-poster.jfif"
+    },    
+    {
+      titulo: "Transport Tycoon",
+      descricao: "Clássico dos jogos de simulação e construção, lançado originalmente em 1990. Nele, o jogador assume o papel de um magnata dos transportes, construindo e gerenciando uma rede de transporte eficiente e lucrativa. O objetivo é conectar cidades e indústrias, transportando passageiros e cargas para gerar lucro e expandir seu império. O jogo oferece uma grande variedade de veículos, como trens, caminhões, navios e aviões, além de permitir a construção de estradas, ferrovias, portos e aeroportos. Transport Tycoon é conhecido por sua jogabilidade envolvente e sua capacidade de proporcionar horas de diversão para os fãs de simulação.",
+      temas: ["transporte", "economia", "logística"],
+      desenvolvedora: "MicroProse",
+      anoLancamento: 1994,
+      avaliacoes: {
+        metacritic: 92,
+        jogadores: 8.7
+      },
+      precoMedio: 14.99,
+      ondeComprar: "GOG",
+      linkCompra: "https://www.gog.com/game/transport_tycoon",
+      tags: "antigo transporte economia logística",
+      poster: "images/transport-tycoon-poster.jpg"
+    },
+    {
+        titulo: "The Sims",
+        descricao: "Revolucionou o gênero dos jogos de simulação de vida ao permitir que os jogadores criassem e controlassem a vida de personagens virtuais, chamados Sims. Lançado em 2000, o jogo oferece uma experiência imersiva, onde os jogadores podem construir casas, estabelecer relacionamentos, escolher carreiras e experimentar diferentes estilos de vida. A série The Sims se tornou um grande sucesso, com milhões de cópias vendidas em todo o mundo.",
+        temas: ["simulação de vida", "criação de personagens", "construção", "relacionamentos"],
+        desenvolvedora: "Maxis",
+        anoLancamento: 2000,
+        avaliacoes: {
+            metacritic: 89,
+            jogadores: 9.2
+        },
+        precoMedio: 19.99,
+        ondeComprar: "Origin",
+        linkCompra: "https://www.origin.com/store/the-sims",
+        tags: "vida personagens personagem construção relacionamentos famoso antigo",
+        poster: "images/the-sims-poster.jpg"
+    },
+    {
+        titulo: "The Sims 2",
+        descricao: "Sequência do popular jogo de simulação de vida. Lançado em 2004, o jogo introduziu novas características, como um sistema de envelhecimento mais detalhado, novas aspirações e desejos para os Sims, e a capacidade de criar bebês e acompanhar seu crescimento. The Sims 2 também expandiu as opções de personalização, permitindo aos jogadores criar Sims ainda mais únicos e personalizados.",
+        temas: ["simulação de vida", "construção", "relacionamentos"],
+        desenvolvedora: "Maxis",
+        anoLancamento: 2004,
+        avaliacoes: {
+          metacritic: 91,
+          jogadores: 8.7
+        },
+        precoMedio: 19.99,
+        ondeComprar: "Origin",
+        linkCompra: "https://www.origin.com/store/the-sims-2",
+        tags: "vida construção relacionamentos personagem personagens famoso antigo",
+        poster: "images/the-sims-2-poster.jpg"
+    },
+    {
+        titulo: "The Sims 3",
+        descricao: "Lançado em 2009 e trouxe uma série de inovações para a franquia. O jogo introduziu um mundo aberto, onde os Sims podem explorar livremente diferentes vizinhanças. Outras novidades incluem a criação de Sims mais detalhada, um sistema de habilidades mais profundo e a capacidade de controlar múltiplos Sims ao mesmo tempo. The Sims 3 também ofereceu uma grande variedade de pacotes de expansão, que adicionaram novas características e conteúdos ao jogo base.",
+        temas: ["simulação de vida", "criação de personagens", "construção", "carreira"],
+        desenvolvedora: "Maxis",
+        anoLancamento: 2009,
+        avaliacoes: {
+            metacritic: 88,
+            jogadores: 8.9
+        },
+        precoMedio: 29.99,
+        ondeComprar: "Origin",
+        linkCompra: "https://www.origin.com/store/the-sims-3",
+        tags: "vida pensonagem personagens construção carreira famoso antigo",
+        poster: "images/the-sims-3-poster.jpg"
+    },
+    {
+        titulo: "The Sims 4",
+        descricao: "O mais recente jogo da franquia, lançado em 2014. Ele continua a oferecer a experiência de criar e controlar a vida de Sims, mas com gráficos aprimorados e novas ferramentas de criação. O jogo introduziu um sistema de emoções mais sofisticado, que influencia o comportamento dos Sims, e novas ferramentas de construção que permitem aos jogadores criar casas e mundos ainda mais personalizados. The Sims 4 também recebeu diversos pacotes de expansão e game packs, que adicionam novos conteúdos e funcionalidades ao jogo.",
+        temas: ["simulação de vida", "criação de personagens", "construção", "emoções"],
+        desenvolvedora: "Maxis",
+        anoLancamento: 2014,
+        avaliacoes: {
+            metacritic: 85,
+            jogadores: 8.2
+        },
+        precoMedio: 39.99,
+        ondeComprar: "Origin",
+        linkCompra: "https://www.origin.com/store/the-sims-4",
+        tags: "vida personagem personagens construção emoções famoso recente",
+        poster: "images/the-sims-4-poster.jpg"
+    },
+    {
+        titulo: "Theme Hospital",
+        descricao: "Clássico dos jogos de simulação e estratégia, lançado originalmente em 1997. Nele, você assume o papel de um administrador de hospital, responsável por construir e gerenciar um hospital, desde a construção de salas e a contratação de funcionários até o tratamento de doenças bizarras e a satisfação dos pacientes. O jogo combina elementos de construção, gerenciamento de recursos e humor, criando uma experiência única e divertida. O objetivo é construir o melhor hospital do mundo, tratando o maior número de pacientes e gerando lucro.",
+        temas: ["gestão de hospitais", "humor", "simulação"],
+        desenvolvedora: "Bullfrog Productions",
+        anoLancamento: 1997,
+        avaliacoes: {
+            metacritic: 92, 
+            jogadores: 9.1 
+        },
+        precoMedio: 19.99, 
+        ondeComprar: "GOG",
+        linkCompra: "https://www.gog.com/game/theme_hospital",
+        tags: "humor DOS antigo famoso",
+        poster: "images/theme-hospital-poster.jpg"
+    },
+    {
+        titulo: "RollerCoaster Tycoon 3",
+        descricao: "Um dos jogos de simulação de parques de diversões mais populares de todos os tempos. Nele, você assume o papel de um magnata dos parques, construindo e gerenciando seu próprio parque de diversões. Você terá a liberdade de criar montanhas-russas personalizadas, construir diversas atrações, gerenciar funcionários e atender às necessidades dos visitantes. O jogo oferece um alto grau de personalização e permite que você crie parques temáticos únicos e memoráveis.",
+        temas: ["construção de parques", "simulação", "entretenimento"],
+        desenvolvedora: "Frontier Developments",
+        anoLancamento: 2004,
+        avaliacoes: {
+            metacritic: 88,
+            jogadores: 8.9
+        },
+        precoMedio: 29.99,
+        ondeComprar: "Steam",
+        linkCompra: "https://store.steampowered.com/app/2200/RollerCoaster_Tycoon_3/",
+        tags: "diversão antigo montanha russa",
+        poster: "images/rct-3-poster.jpg"
+    },
+    {
+        titulo: "Cities: Skylines 2",
+        descricao: "Aguardada sequência do popular simulador de construção de cidades. O jogo promete levar a experiência de construir e gerenciar uma cidade a um novo nível, com gráficos aprimorados, ferramentas mais intuitivas e um mundo mais vivo e dinâmico. Os jogadores poderão construir cidades gigantescas, lidar com desafios complexos como o tráfego, a poluição e a economia, e criar comunidades prósperas e sustentáveis.",
+        temas: ["construção de cidades", "economia", "transporte"],
+        desenvolvedora: "Colossal Order",
+        anoLancamento: 2023, 
+        avaliacoes: {
+            metacritic: 87, 
+            jogadores: 9.1 
+            },
+        precoMedio: 49.99,
+        ondeComprar: "Steam",
+        linkCompra: "https://store.steampowered.com/app/1526310/Cities_Skylines_2/",
+        tags: "economia transporte famoso recente",
+        poster: "images/cities-skylines-II-poster.png"
+    },
+    {
+        titulo: "Two Point Campus",
+        descricao: "Jogo de simulação de gestão universitária desenvolvido pelos criadores da série Theme Hospital. Nele, você construirá e gerenciará uma universidade, desde a construção de salas de aula e dormitórios até a criação de cursos e a organização de eventos. O jogo combina humor, estratégia e simulação, permitindo que você crie uma universidade única e memorável.",
+        temas: ["gestão de universidades", "humor", "simulação"],
+        desenvolvedora: "Two Point Studios",
+        anoLancamento: 2022,
+        avaliacoes: {
+            metacritic: 85,
+            jogadores: 8.8
+        },
+        precoMedio: 39.99,
+        ondeComprar: "Steam",
+        linkCompra: "https://store.steampowered.com/app/1602480/Two_Point_Campus/",
+        tags: "recente famoso",
+        poster: "images/two-point-campus-poster.jpg"
+    },
+    {
+        titulo: "Terra Nil",
+        descricao: "Jogo de estratégia de construção de mundo que oferece uma abordagem única para a simulação. Ao invés de construir cidades e indústrias, seu objetivo é restaurar um ecossistema devastado, transformando uma terra árida em um paraíso natural. Você utilizará tecnologias avançadas para limpar a poluição, plantar árvores, criar habitats para animais e, finalmente, remover todas as suas construções, deixando para trás um ecossistema próspero e autossustentável.",
+        temas: ["ecologia", "estratégia", "restauração ambiental"],
+        desenvolvedora: "Free Lives",
+        anoLancamento: 2022,
+        avaliacoes: {
+            metacritic: 89,
+            jogadores: 9.2
+        },
+        precoMedio: 29.99,
+        ondeComprar: "Steam",
+        linkCompra: "https://store.steampowered.com/app/1620980/Terra_Nil/",
+        tags: "restauração meio ambiente estratégia recente",
+        poster: "images/terra-Nil-poster.jpg"
+    }
+];
